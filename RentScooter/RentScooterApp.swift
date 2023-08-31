17dcd90453084d86b40d14609e34b3745fcd30da
A <A>
2023-09-01 00:16:35 +0300

    Initial Commit

---
+++ RentScooter/RentScooterApp.swift
@@ -0,0 +1,17 @@
+//
+//  RentScooterApp.swift
+//  RentScooter
+//
+//  Created by Вяткин Александр on 01.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct RentScooterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
